9c80a3b98c76c6f6e38c57b24487e28f71e6a84c
Shawn Williams <shawn79williams@yahoo.com>
2018-03-31 14:50:33 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io();
 socket.on('connect', function() {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-        from: 'Shawn Client',
-        text: 'Text from client'
-    });
+    // socket.emit('createMessage', {
+    //     from: 'Shawn Client',
+    //     text: 'Text from client'
+    // });
 });
 
 socket.on('disconnect', function() {
