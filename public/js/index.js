582f8677ced2ac8e56eb979e2b0128f24e7a0993
Shawn Williams <shawn79williams@yahoo.com>
2018-04-03 23:16:09 -0400

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom() {
+    // Selectors
+    var messages = $('#messages');
+    var newMessage = messages.children('li:last-child')
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('Connected to server');
 
@@ -23,6 +39,7 @@ socket.on('newMessage', function(message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
     // console.log('New message', message);
     // var li = jQuery('<li></li>');
     // li.text(`${message.from} ${formattedTime}: ${message.text}`)
@@ -39,6 +56,7 @@ socket.on('newLocationMessage', function(message){
         createdAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
     // var li = jQuery(`<li></li>`);
     // var a = jQuery('<a target="_blank">Location</a>')
     // li.text(`${message.from} ${formattedTime}: `);
